db7e615482527257dd91600bbd6f838e8d5ce5e1
s.shtukar <u1111887@internal.imsglobal.com>
2023-02-14 21:15:29 +0400

    errors

--- src/main/java/tools/base/BaseTest.java
+++ src/main/java/tools/base/BaseTest.java
@@ -3,19 +3,15 @@ package tools.base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
     private static final WebDriver driver = new ChromeDriver();
 
-    @BeforeTest
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver.get("https://store.steampowered.com/");
     }
 
-    @AfterTest
     public void tearDown() {
         driver.close();
         driver.quit();
