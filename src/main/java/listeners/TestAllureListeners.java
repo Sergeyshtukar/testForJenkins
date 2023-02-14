91596141552e39a875d738287c12a6ee23eb68bb
s.shtukar <u1111887@internal.imsglobal.com>
2023-02-14 22:55:39 +0400

    code refactored

--- src/main/java/listeners/TestAllureListeners.java
+++ src/main/java/listeners/TestAllureListeners.java
@@ -3,21 +3,38 @@ package listeners;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
 import tools.browserFactory.DriverStart;
 
-public class TestAllureListeners {
+public class TestAllureListeners extends TestListenerAdapter {
 
     @Attachment(value = "Page screenshot", type = "image/png")
     public byte[] saveScreenshot() {
         return ((TakesScreenshot) DriverStart.getInstance().getDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
-    public void onTestFailure() {
-        WebDriver driver = DriverStart.getInstance().getDriver();
-        // Allure ScreenShotRobot
-        if (driver instanceof WebDriver) {
-            saveScreenshot();
+    private int m_count = 0;
+
+    @Override
+    public void onTestFailure(ITestResult tr) {
+        saveScreenshot();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult tr) {
+        log("S");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult tr) {
+        log(".");
+    }
+
+    private void log(String string) {
+        System.out.print(string);
+        if (++m_count % 40 == 0) {
+            System.out.println("");
         }
     }
 }
